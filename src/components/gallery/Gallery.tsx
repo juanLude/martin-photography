9ff920a2e2cf85a35282c867d4330b62d07bd0a7
Juan Ludevid <jmludevid@hotmail.com>
2023-10-05 22:17:06 +1100

    change nav bar / SideMenu component

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,28 +1,28 @@
-import  { useState, useEffect, useCallback } from 'react'
-import GalleryData from '../../data/GalleryData';
-import ReactPaginate from 'react-paginate';
-import Images from './Images'
-import ResetLocation from '../../helpers/ResetLocation';
-import 'react-slideshow-image/dist/styles.css'
+import { useState, useEffect, useCallback } from "react";
+import GalleryData from "../../data/GalleryData";
+import ReactPaginate from "react-paginate";
+import Images from "./Images";
+import ResetLocation from "../../helpers/ResetLocation";
+import "react-slideshow-image/dist/styles.css";
 
 export type imagesToShow = {
-  id: number
-  src: string
-  alt: string
-}
+  id: number;
+  src: string;
+  alt: string;
+};
 
 export interface ImagesProp {
-  imagesToShow: imagesToShow[]
+  imagesToShow: imagesToShow[];
   toggle: () => void;
   setModalImg: (imgIndex: number) => void;
-
 }
 
 const Gallery = () => {
-  
   const [itemOffset, setItemOffset] = useState(0);
   const [endOffset, setEndOffset] = useState(itemOffset + 12);
-  const [currentBlogPosts, setcurrentBlogPosts] = useState([...GalleryData].slice(itemOffset, endOffset));
+  const [currentBlogPosts, setcurrentBlogPosts] = useState(
+    [...GalleryData].slice(itemOffset, endOffset)
+  );
   const [isOpen, setisOpen] = useState(false);
   const [targetImg, setTargetImg] = useState(0);
 
@@ -31,9 +31,11 @@ const Gallery = () => {
   }, [currentBlogPosts.length]);
 
   const handlePrevClick = useCallback(() => {
-    setTargetImg((prevImg) => (prevImg - 1 + currentBlogPosts.length) % currentBlogPosts.length);
+    setTargetImg(
+      (prevImg) =>
+        (prevImg - 1 + currentBlogPosts.length) % currentBlogPosts.length
+    );
   }, [currentBlogPosts.length]);
-
 
   const handlePageClick = (event: any) => {
     setItemOffset((event.selected * 12) % GalleryData.length);
@@ -46,40 +48,43 @@ const Gallery = () => {
 
   const setModalImg = (imgIndex: number) => {
     setTargetImg(imgIndex);
-  }
+  };
 
   useEffect(() => {
-    document.title = "Gallery | Travel with Catherine";
+    document.title = "Martin Scarponi | Photography";
     setEndOffset(itemOffset + 12);
     setcurrentBlogPosts([...GalleryData].slice(itemOffset, endOffset));
   }, [setEndOffset, endOffset, itemOffset]);
 
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      const target = event.target as Element;
+      const nextBtn = target.getAttribute("data-type") === "next";
+      const prevBtn = target.getAttribute("data-type") === "prev";
 
-    useEffect(() => {
-      const handleClick = (event: MouseEvent) => {
-        const target = event.target as Element;
-        const nextBtn = target.getAttribute('data-type') === 'next';
-        const prevBtn = target.getAttribute('data-type') === 'prev';
-    
-        if (nextBtn) {
-          handleNextClick();
-        }
-        if (prevBtn) {
-          handlePrevClick();
-        }
-      };
-    
-      window.addEventListener('click', handleClick);
-    
-      return () => {
-        // Clean up the event listener when the component unmounts
-        window.removeEventListener('click', handleClick);
-      };
-    }, [handleNextClick, handlePrevClick]);
-    
+      if (nextBtn) {
+        handleNextClick();
+      }
+      if (prevBtn) {
+        handlePrevClick();
+      }
+    };
+
+    window.addEventListener("click", handleClick);
+
+    return () => {
+      // Clean up the event listener when the component unmounts
+      window.removeEventListener("click", handleClick);
+    };
+  }, [handleNextClick, handlePrevClick]);
+
   return (
     <article className="sm:col-span-5 md:col-span-4 min-h-screen scrollbar-hide">
-      <Images imagesToShow={currentBlogPosts} toggle={toggle} setModalImg={setModalImg} />
+      <Images
+        imagesToShow={currentBlogPosts}
+        toggle={toggle}
+        setModalImg={setModalImg}
+      />
       <section className="flex flex-col items-center w-full">
         <ReactPaginate
           className="pagination"
@@ -91,41 +96,49 @@ const Gallery = () => {
           previousLabel="&#60;"
         />
       </section>
-      {isOpen ?
+      {isOpen ? (
         //Modal
-        <section className={`img-modal w-screen h-screen fixed z-30 top-0 left-0 flex  justify-center bg-rgba(255, 255, 255, .15) backdrop-blur-sm ${isOpen ? 'flex' : 'hidden'}`}>
-        <button onClick={toggle} className="absolute flex items-center justify-center z-40 text-2xl text-gray-200 text-bold p-2 bg-black rounded-full w-10 h-10 right-4 top-4 hover:-rotate-90 transition duration-500">
-          X
-        </button>
-        <button
-          onClick={handlePrevClick}
-          className="absolute flex items-center justify-center z-40 left-8 top-1/2 text-xl text-gray-200 text-bold p-6 bg-black rounded-full w-10 h-10 hover:bg-opacity-75 transition"
-          data-type="prev"
+        <section
+          className={`img-modal w-screen h-screen fixed z-30 top-0 left-0 flex  justify-center bg-rgba(255, 255, 255, .15) backdrop-blur-sm ${
+            isOpen ? "flex" : "hidden"
+          }`}
         >
-          &lt;
-        </button>
-        <section className="slider-container relative w-4/5 flex items-center">
-          {currentBlogPosts.map((image, index) => (
-            <img
-              key={image.id}
-              className={`slider-slide ${targetImg === index ? 'active' : 'hidden'} object-cover object-center w-full sm:h-2/3 md:h-3/4`}
-              src={image.src}
-              alt=''
-            />
-          ))}
+          <button
+            onClick={toggle}
+            className="absolute flex items-center justify-center z-40 text-2xl text-gray-200 text-bold p-2 bg-black rounded-full w-10 h-10 right-4 top-4 hover:-rotate-90 transition duration-500"
+          >
+            X
+          </button>
+          <button
+            onClick={handlePrevClick}
+            className="absolute flex items-center justify-center z-40 left-8 top-1/2 text-xl text-gray-200 text-bold p-6 bg-black rounded-full w-10 h-10 hover:bg-opacity-75 transition"
+            data-type="prev"
+          >
+            &lt;
+          </button>
+          <section className="slider-container relative w-4/5 flex items-center">
+            {currentBlogPosts.map((image, index) => (
+              <img
+                key={image.id}
+                className={`slider-slide ${
+                  targetImg === index ? "active" : "hidden"
+                } object-cover object-center w-full sm:h-2/3 md:h-3/4`}
+                src={image.src}
+                alt=""
+              />
+            ))}
+          </section>
+          <button
+            onClick={handleNextClick}
+            className="absolute z-40 flex items-center justify-center right-8 top-1/2 text-xl text-gray-200 text-bold p-6 bg-black rounded-full w-10 h-10 hover:bg-opacity-75 transition"
+            data-type="next"
+          >
+            &gt;
+          </button>
         </section>
-        <button
-          onClick={handleNextClick}
-          className="absolute z-40 flex items-center justify-center right-8 top-1/2 text-xl text-gray-200 text-bold p-6 bg-black rounded-full w-10 h-10 hover:bg-opacity-75 transition"
-          data-type="next"
-        >
-          &gt;
-        </button>
-      </section>
-       :
-        null}
+      ) : null}
     </article>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
--- src/components/sideMenu/SideMenu.tsx
+++ src/components/sideMenu/SideMenu.tsx
@@ -1,89 +1,200 @@
-import { useRef, useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import hiddenMenuIcon from '../../assets/images/burger-passive.png'
-import activeMenuIcon from '../../assets/images/burger-active.png'
-import Socials from './Socials'
-import CSS from 'csstype'
+// import { useRef, useState } from "react";
+// import { NavLink } from "react-router-dom";
+// import hiddenMenuIcon from "../../assets/images/burger-passive.png";
+// import activeMenuIcon from "../../assets/images/burger-active.png";
+// import Socials from "./Socials";
+// import CSS from "csstype";
+
+// const activeMenuStyles: CSS.Properties = {
+//   textDecoration: "none",
+//   color: "rgb(37, 99, 235)",
+// };
+
+// const SideMenu = () => {
+//   const [hiddenMenu, setHiddenMenu] = useState<boolean>(true);
+//   const ref = useRef<HTMLImageElement>(null);
+
+//   function ToggleMenu() {
+//     setHiddenMenu(!hiddenMenu);
+//   }
+//   function RemoveMenu() {
+//     setHiddenMenu(true);
+//     window.scrollTo(0, 0);
+//   }
+
+//   return (
+//     <header className="sm:relative flex flex-col min-h-screen sm:col-span-1 md:col-span-2 items-center absolute">
+//       <img
+//         className="burger-icon w-10 fixed cursor-pointer md:hidden z-50 sm:ml-2 ml-12"
+//         onClick={ToggleMenu}
+//         ref={ref}
+//         src={hiddenMenu ? hiddenMenuIcon : activeMenuIcon}
+//         alt="Toggle menu"
+//       />
+//       <nav
+//         className={`main-menu${
+//           hiddenMenu ? `` : ` show-menu`
+//         } flex-col items-center fixed mt-60 text-2xl gap-5 text-slate-600 hidden md:flex`}
+//       >
+//         <h1 className="sm:text-xl md:text-3xl text-center uppercase text-black font-light">
+//           Martin Scarponi<br></br>
+//           <span className="font-semibold font-namefont text-5xl">
+//             Photography
+//           </span>
+//         </h1>
+//         <NavLink
+//           className="hover:text-blue-600 smooth-transition text-xl"
+//           to="/"
+//           onClick={() => {
+//             RemoveMenu();
+//           }}
+//         >
+//           Gallery
+//         </NavLink>
+//         <NavLink
+//           className="hover:text-blue-600 smooth-transition text-xl"
+//           to="/blog"
+//           onClick={() => {
+//             RemoveMenu();
+//           }}
+//           style={({ isActive }) => (isActive ? activeMenuStyles : {})}
+//         >
+//           Blog
+//         </NavLink>
+//         <NavLink
+//           className="hover:text-blue-600 smooth-transition text-xl"
+//           to="/about"
+//           onClick={() => {
+//             RemoveMenu();
+//           }}
+//           style={({ isActive }) => (isActive ? activeMenuStyles : {})}
+//         >
+//           About
+//         </NavLink>
+//         <NavLink
+//           className="hover:text-blue-600 smooth-transition text-xl"
+//           to="/contact"
+//           onClick={() => {
+//             RemoveMenu();
+//           }}
+//           style={({ isActive }) => (isActive ? activeMenuStyles : {})}
+//         >
+//           Contact
+//         </NavLink>
+//         <Socials />
+//         <p className="text-sm">&copy; 2022-2023</p>
+//       </nav>
+//     </header>
+//   );
+// };
+
+// export default SideMenu;
+
+import { useRef, useState } from "react";
+import { NavLink } from "react-router-dom";
+import hiddenMenuIcon from "../../assets/images/burger-passive.png";
+import activeMenuIcon from "../../assets/images/burger-active.png";
+import CSS from "csstype";
 
 const activeMenuStyles: CSS.Properties = {
-  textDecoration: 'none',
-  color: 'rgb(37, 99, 235)',
-}
+  textDecoration: "none",
+  color: "rgb(37, 99, 235)",
+};
 
 const SideMenu = () => {
-  const [hiddenMenu, setHiddenMenu] = useState<boolean>(true)
-  const ref = useRef<HTMLImageElement>(null)
+  const [hiddenMenu, setHiddenMenu] = useState<boolean>(true);
+  const ref = useRef<HTMLImageElement>(null);
 
   function ToggleMenu() {
     setHiddenMenu(!hiddenMenu);
   }
+
   function RemoveMenu() {
-    setHiddenMenu(true)
-    window.scrollTo(0, 0)
+    setHiddenMenu(true);
+    window.scrollTo(0, 0);
   }
 
   return (
-    <header className="sm:relative flex flex-col min-h-screen sm:col-span-1 md:col-span-2 items-center absolute">
-      <img
-        className="burger-icon w-10 fixed cursor-pointer md:hidden z-50 sm:ml-2 ml-12"
-        onClick={ToggleMenu}
-        ref={ref}
-        src={hiddenMenu ? hiddenMenuIcon : activeMenuIcon}
-        alt="Toggle menu"
-      />
-      <nav
+    <nav className={`fixed top-0 left-0 right-0 bg-white shadow-md z-50`}>
+      <div className="flex justify-between items-center py-2 px-4">
+        {/* <h1 className="text-xl sm:text-2xl md:text-3xl text-black font-light">
+          <span className="text-4xl sm:text-5xl font-semibold font-namefont">
+            Martin Scarponi
+          </span>
+          <br />
+          Photography
+        </h1> */}
+        <img
+          className="burger-icon w-10 cursor-pointer md:hidden"
+          onClick={ToggleMenu}
+          ref={ref}
+          src={hiddenMenu ? hiddenMenuIcon : activeMenuIcon}
+          alt="Toggle menu"
+        />
+      </div>
+      <div
         className={`main-menu${
           hiddenMenu ? `` : ` show-menu`
-        } flex-col items-center fixed mt-60 text-2xl gap-5 text-slate-600 hidden md:flex`}
+        } flex items-center text-xl gap-5 text-slate-600 hidden md:flex`}
       >
-        <h1 className="sm:text-xl md:text-3xl text-center uppercase text-black font-light">
-          Travel with<br></br>
-          <span className="font-semibold font-namefont text-5xl">Catherine</span>
-        </h1>
         <NavLink
-          className="hover:text-blue-600 smooth-transition text-xl"
+          className="hover:text-blue-600 smooth-transition"
           to="/"
           onClick={() => {
-            RemoveMenu()
+            RemoveMenu();
+          }}
+        >
+          <h1 className="text-xl sm:text-2xl md:text-3xl text-black font-light">
+            <span className="text-4xl sm:text-5xl font-semibold font-namefont">
+              Martin Scarponi
+            </span>
+            {/* <br />
+            Photography */}
+          </h1>
+        </NavLink>
+
+        <NavLink
+          className="hover:text-blue-600 smooth-transition"
+          to="/"
+          onClick={() => {
+            RemoveMenu();
           }}
         >
           Gallery
         </NavLink>
         <NavLink
-          className="hover:text-blue-600 smooth-transition text-xl"
+          className="hover:text-blue-600 smooth-transition"
           to="/blog"
           onClick={() => {
-            RemoveMenu()
+            RemoveMenu();
           }}
           style={({ isActive }) => (isActive ? activeMenuStyles : {})}
         >
           Blog
         </NavLink>
         <NavLink
-          className="hover:text-blue-600 smooth-transition text-xl"
+          className="hover:text-blue-600 smooth-transition"
           to="/about"
           onClick={() => {
-            RemoveMenu()
+            RemoveMenu();
           }}
           style={({ isActive }) => (isActive ? activeMenuStyles : {})}
         >
           About
         </NavLink>
         <NavLink
-          className="hover:text-blue-600 smooth-transition text-xl"
+          className="hover:text-blue-600 smooth-transition"
           to="/contact"
           onClick={() => {
-            RemoveMenu()
+            RemoveMenu();
           }}
           style={({ isActive }) => (isActive ? activeMenuStyles : {})}
         >
           Contact
         </NavLink>
-        <Socials />
-        <p className="text-sm">&copy; 2022-2023</p>
-      </nav>
-    </header>
-  )
-}
+      </div>
+    </nav>
+  );
+};
 
-export default SideMenu
+export default SideMenu;
